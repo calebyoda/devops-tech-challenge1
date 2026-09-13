5bc35d7b984ebbc3e7ceb5167cad250f17139e85
Caleb Yoda <caleb.as.yoda@outlook.com>
2026-09-12 22:16:36 -0400

    Add trailing slash to API_URL to match ALB /api/* listener rule

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
-const API_URL = "http://devops-challenge-alb-1126066491.us-east-1.elb.amazonaws.com/api";
+const API_URL = "http://devops-challenge-alb-1126066491.us-east-1.elb.amazonaws.com/api/";
 
 export default API_URL;
